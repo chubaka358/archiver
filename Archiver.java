cd42d5cf14d5820a440f9e8941da63a308cbac04
o/ <chubaka358@MacBook-Pro-o.local>
2019-10-16 15:27:02 +0300

    step 8

--- Archiver.java
+++ Archiver.java
@@ -1,8 +1,10 @@
 package com.javarush.task.task31.task3110;
 
 import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,12 +12,27 @@ import java.nio.file.Paths;
 public class Archiver {
 	public static void main(String[] args) throws Exception {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Введите полный путь к архиву");
-		Path path = Paths.get(reader.readLine());
-		ZipFileManager zipFileManager = new ZipFileManager(path);
-		System.out.println("Введите полный путь к файлу, который необходимо архивировать");
-		zipFileManager.createZip(Paths.get(reader.readLine()));
-		ExitCommand exitCommand = new ExitCommand();
-		exitCommand.execute();
+		Operation operation = null;
+		do{
+			try {
+				operation = askOperation();
+				CommandExecutor.execute(operation);
+			} catch (WrongZipFileException exception){
+				ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+			} catch (Exception exception){
+				ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+			}
+		} while	(operation != operation.EXIT);
+	}
+
+	public static Operation askOperation() throws IOException {
+		ConsoleHelper.writeMessage(Operation.CREATE.ordinal() + " - упаковать файлы в архив");
+		ConsoleHelper.writeMessage(Operation.ADD.ordinal() + " - добавить файл в архив");
+		ConsoleHelper.writeMessage(Operation.REMOVE.ordinal() + " - удалить файл из архива");
+		ConsoleHelper.writeMessage(Operation.EXTRACT.ordinal() + " - распаковать архив");
+		ConsoleHelper.writeMessage(Operation.CONTENT.ordinal() + " - просмотреть содержимое архива");
+		ConsoleHelper.writeMessage(Operation.EXIT.ordinal() + " - выход");
+		int operationNumber = ConsoleHelper.readInt();
+		return Operation.values()[operationNumber];
 	}
 }
--- command/ZipAddCommand.java
+++ command/ZipAddCommand.java
@@ -3,6 +3,5 @@ package com.javarush.task.task31.task3110.command;
 public class ZipAddCommand extends ZipCommand {
 	@Override
 	public void execute() throws Exception {
-
 	}
 }
