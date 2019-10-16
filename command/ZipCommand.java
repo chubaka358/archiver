ebf14bcc05eb16c206c2beaf9301c36eb639f10b
o/ <chubaka358@MacBook-Pro-o.local>
2019-10-16 17:39:23 +0300

    step 12

--- command/ZipCommand.java
+++ command/ZipCommand.java
@@ -6,12 +6,11 @@ import com.javarush.task.task31.task3110.ZipFileManager;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public abstract class ZipCommand implements Command{
-	public ZipFileManager getZipFileManager() throws Exception{
-		ConsoleHelper.writeMessage("Введите полный путь файла архива");
-		String archivePathName = ConsoleHelper.readString();
-		Path archivePath = Paths.get(archivePathName);
-		ZipFileManager zipFileManager = new ZipFileManager(archivePath);
-		return zipFileManager;
-	}
+public abstract class ZipCommand implements Command {
+
+    public ZipFileManager getZipFileManager() throws Exception{
+        ConsoleHelper.writeMessage("Введите полный путь файла архива:");
+        Path zipPath = Paths.get(ConsoleHelper.readString());
+        return new ZipFileManager(zipPath);
+    }
 }
