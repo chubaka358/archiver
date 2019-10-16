99ae65afcd74e1e88e59e328cec5bf48d752ca68
o/ <chubaka358@MacBook-Pro-o.local>
2019-10-16 16:28:05 +0300

    step 9

--- Archiver.java
+++ Archiver.java
@@ -1,38 +1,37 @@
 package com.javarush.task.task31.task3110;
 
-import com.javarush.task.task31.task3110.command.ExitCommand;
 import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Archiver {
-	public static void main(String[] args) throws Exception {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		Operation operation = null;
-		do{
-			try {
-				operation = askOperation();
-				CommandExecutor.execute(operation);
-			} catch (WrongZipFileException exception){
-				ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
-			} catch (Exception exception){
-				ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
-			}
-		} while	(operation != operation.EXIT);
-	}
+    public static void main(String[] args) throws IOException {
 
-	public static Operation askOperation() throws IOException {
-		ConsoleHelper.writeMessage(Operation.CREATE.ordinal() + " - упаковать файлы в архив");
-		ConsoleHelper.writeMessage(Operation.ADD.ordinal() + " - добавить файл в архив");
-		ConsoleHelper.writeMessage(Operation.REMOVE.ordinal() + " - удалить файл из архива");
-		ConsoleHelper.writeMessage(Operation.EXTRACT.ordinal() + " - распаковать архив");
-		ConsoleHelper.writeMessage(Operation.CONTENT.ordinal() + " - просмотреть содержимое архива");
-		ConsoleHelper.writeMessage(Operation.EXIT.ordinal() + " - выход");
-		int operationNumber = ConsoleHelper.readInt();
-		return Operation.values()[operationNumber];
-	}
+        Operation operation = null;
+        do {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
+
+        } while (operation != Operation.EXIT);
+    }
+
+
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("");
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage(String.format("\t %d - упаковать файлы в архив", Operation.CREATE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - добавить файл в архив", Operation.ADD.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - удалить файл из архива", Operation.REMOVE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - распаковать архив", Operation.EXTRACT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - просмотреть содержимое архива", Operation.CONTENT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - выход", Operation.EXIT.ordinal()));
+
+        return Operation.values()[ConsoleHelper.readInt()];
+    }
 }
